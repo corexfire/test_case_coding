97a91b604533eda79dd2f49dc6168a452cdda4fa
Muhammad Zubair <zubair.mi45@gmail.com>
2024-05-04 05:18:56 +0700

    Update db.sql

--- sql/db.sql
+++ sql/db.sql
@@ -1,10 +1,17 @@
 CREATE DATABASE IF NOT EXISTS apbatech_test;
 USE apbatech_test;
 
-CREATE TABLE IF NOT EXISTS apbatech_test (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    mobile_number VARCHAR(20) NOT NULL,
-    foto VARCHAR(255) NOT NULL
-);
+CREATE TABLE `users` (
+	`id` INT(10) NOT NULL AUTO_INCREMENT,
+	`email` VARCHAR(255) NOT NULL ,
+	`password` VARCHAR(255) NOT NULL ,
+	`mobile_number` VARCHAR(20) NOT NULL ,
+	`foto` LONGBLOB NOT NULL,
+	`createdAt` TIMESTAMP NULL DEFAULT NULL,
+	`updatedAt` TIMESTAMP NULL DEFAULT NULL,
+	PRIMARY KEY (`id`) USING BTREE,
+	UNIQUE INDEX `email` (`email`) USING BTREE
+)
+ENGINE=InnoDB
+;
+
